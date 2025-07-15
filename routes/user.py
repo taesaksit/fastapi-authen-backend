f1bc13bea04467051bf2804cd8d1f8cecf4f8ec6
taepc <taesaksit09@gmail.com>
2025-07-15 16:34:10 +0700

    change import name

--- routes/user.py
+++ routes/user.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from crud import user as crud
+from repository import user as crud
 from database import get_db
 from schemas import user as schemas
 from schemas.response import ResponseSchema
