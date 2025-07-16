308c58332a487f2e353e50f08dd75feb6e9d141d
taepc <taesaksit09@gmail.com>
2025-07-16 21:41:26 +0700

    update security

--- utils/security.py
+++ utils/security.py
@@ -19,7 +19,7 @@ def verify_password(plain_password: str, hashed_password: str):
 def create_access_token(data: dict, expires_delta: timedelta = None):
 
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=15))
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
